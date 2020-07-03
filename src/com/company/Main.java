bd18c15198c05394fe6017ac0d75335b7a7c400a
Vivekanand P V <vivekanandpv@outlook.com>
2020-07-03 20:16:20 +0530

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,53 +1,76 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        //  Array is the most basic data structure that is implemented
-        //  directly by the language. This as close to the machine as possible.
-        //  But this is fixed in size (cannot grow and shrink). The size of the array
-        //  has to be a compile-time constant. Array is non-generic
+        //  Utility static methods: since Java 9
+        //  Immutable (unmodifiable) list of up to 10 items
+        List<Integer> integerList = List.of(0, 10, 8, 7, 11, 12, 5, 4, 25);
 
-        int[] intArray = {0, 1, 2, 3, 4};
+        //  Immutable (unmodifiable) set of up to 10 items
+        Set<Integer> integerSet = Set.of(0, 10, 8, 7, 11, 12, 5, 4, 25);
 
-        //  We obviously want the performance of raw array, but we also want:
-        //  1. dynamic size
-        //  2. generic support
-        //  3. convenient API for element manipulation
+        //  Immutable (unmodifiable) map of up to 10 pairs
+        Map<String, Double> map = Map.of("pi", 3.1415, "e", 2.71828);
 
-        //  There are two candidate data-structures: ArrayList and LinkedList
-        //  ArrayList elements are in contiguous blocks in memory, hence providing
-        //  faster index-based access. As the consequence right-shift and left-shift
-        //  operations are inefficient. LinkedList in inefficient in random-access
-        //  but efficient in addition/removal.
+        //  Sorting
+        //  1. Using Java 8 Stream API
+        List<Integer> sortedList = integerList
+                .stream()   //  create stream
+                .sorted()   //  sort
+                .collect(Collectors.toList());  //  collect
 
-        //  In Java collection API, we do not program to classes, but to interfaces.
-        //  Therefore, whether ArrayList or LinkedList, we use the List<E> interface.
-
-        List<Integer> integerList1 = new ArrayList<>();
-        List<Integer> integerList2 = new LinkedList<>();
-
-        //  List<E>: https://docs.oracle.com/en/java/javase/14/docs/api/java.base/java/util/List.html
-
-        //  Actions: Mutators
-        integerList1.add(100);
-        integerList1.remove(0);
-        integerList1.addAll(List.of(1, 2, 3, 4));
-        integerList1.add(2, 200);
-
-        //  Actions: Enquirers
-        int a = integerList1.get(0);    //  safe?
-        int b = integerList1.indexOf(200);  //  200 is now of Integer type
-        int size = integerList1.size();
-        boolean contains200 = integerList1.contains(200);
-
-        //  Actions: Iterator
-        for(Integer i: integerList1) {
-            System.out.println("Element: " + i);
+        System.out.println("----------- Natural Sort -----------");
+        for(Integer i: sortedList) {
+            System.out.println(i);
         }
+
+        //  2. Customizing sort order using stream API
+        List<Integer> reverseSortedList = integerList
+                .stream()   //  create stream
+                .sorted((a, b) -> b - a)   //  sort
+                .collect(Collectors.toList());  //  collect
+
+        System.out.println("----------- Reverse Sort -----------");
+        for(Integer i: reverseSortedList) {
+            System.out.println(i);
+        }
+
+        //  3. Using algorithms from Collections API
+        Collections.sort(reverseSortedList);
+        System.out.println("----------- Natural Sort (of reversed) -----------");
+        for(Integer i: reverseSortedList) {
+            System.out.println(i);
+        }
+
+        //  4. Customizing the Collections API algorithm
+        Collections.sort(sortedList, Collections.reverseOrder());
+        System.out.println("----------- Reverse Sort - Algorithm (of sorted) -----------");
+        for(Integer i: sortedList) {
+            System.out.println(i);
+        }
+
+        //  4. Customizing the Collections API algorithm using custom comparator
+        Collections.sort(sortedList, (a, b) -> a.compareTo(b));
+        System.out.println("----------- Natural Sort - Custom (of reverse sorted) -----------");
+        for(Integer i: sortedList) {
+            System.out.println(i);
+        }
+
+        //  5. List<E> sort
+        sortedList.sort((a, b) -> b.compareTo(a));
+        System.out.println("----------- Reverse Sort - List.sort -----------");
+        for(Integer i: sortedList) {
+            System.out.println(i);
+        }
+
+        //  Search
+        System.out.println("----------- Search -----------");
+        int indexOf10 = sortedList.indexOf(10); //  -1 if not found
+        System.out.println("10 is found at: " + indexOf10);
+
     }
 }
